bc303064bb9a17cbe107cfe4b9c329ec40395a1e
admin <admin@-22>
2012-10-24 18:16:41 +0900

    ㅁㄴㅇㄹ

--- src/gms/hs/kr/MainActivity.java
+++ src/gms/hs/kr/MainActivity.java
@@ -3,6 +3,7 @@ package gms.hs.kr;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.webkit.WebView;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +11,8 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_student);
+        WebView w = (WebView) findViewById(R.id.webView1);
+        w.loadUrl("file:///android_asset/street_bg.html"); //
     }
 
     @Override
